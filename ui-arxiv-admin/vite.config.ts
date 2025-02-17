d507c159cd8f3916912148cc37dfa963abb54499
Naoyuki Tai <ntai@arxiv.org>
2025-02-17 10:36:28 -0500

    another runtime fix.

--- ui-arxiv-admin/vite.config.ts
+++ ui-arxiv-admin/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/admin-console/',
+  server: {
+    port: 21511,
+    host: '0.0.0.0',
+    allowedHosts: ['0.0.0.0', '127.0.0.1', 'localhost', 'localhost.arxiv.org'],
+  },
   plugins: [react()],
   build: {
     outDir: 'dist',
