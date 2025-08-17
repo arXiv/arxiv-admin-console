ab65fb49fc48305c987cb7311d96c8c375599618
Naoyuki Tai <ntai@arxiv.org>
2025-08-16 20:11:16 -0400

    Well, put it back...

--- api_arxiv_admin/arxiv_admin_api/endorsements.py
+++ api_arxiv_admin/arxiv_admin_api/endorsements.py
@@ -32,7 +32,7 @@ logger = logging.getLogger(__name__)
 router = APIRouter(dependencies=[Depends(is_any_user)], prefix="/endorsements")
 
 
-@router.get('')
+@router.get('/')
 async def list_endorsements(
         response: Response,
         _sort: Optional[str] = Query("issued_when", description="sort by"),
@@ -353,7 +353,7 @@ class EndorsementCreationModel(BaseModel):
     comment: Optional[str] = None
 
 
-@router.post('', description="Create a new endorsement (admin user only)")
+@router.post('/', description="Create a new endorsement (admin user only)")
 async def create_endorsement(
         body: EndorsementCreationModel,
         current_user: ArxivUserClaims = Depends(get_authn_user),
