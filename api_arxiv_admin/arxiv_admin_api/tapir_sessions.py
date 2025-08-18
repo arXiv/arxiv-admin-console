883641a6661d12ae6f924f88d0570265607b2456
Naoyuki Tai <ntai@arxiv.org>
2025-08-18 17:43:55 -0400

    Tapir session interface change

--- api_arxiv_admin/arxiv_admin_api/tapir_sessions.py
+++ api_arxiv_admin/arxiv_admin_api/tapir_sessions.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 router = APIRouter(dependencies=[Depends(is_admin_user)], prefix="/tapir_sessions")
 
 class TapirSessionModel(BaseModel):
-    id: int
+    id: Optional[int] = None
     user_id: int
     last_reissue: Optional[datetime.datetime]
     start_time: Optional[datetime.datetime]
@@ -59,6 +59,7 @@ async def list_tapir_sessions(
         _start: Optional[int] = Query(0, alias="_start"),
         _end: Optional[int] = Query(100, alias="_end"),
         user_id: Optional[int] = Query(None, description="User id"),
+        is_open: Optional[bool] = Query(None, description="Open sessions"),
         id: Optional[List[int]] = Query(None, description="List of user IDs to filter by"),
         preset: Optional[str] = Query(None),
         start_date: Optional[datetime.date] = Query(None, description="Start date for filtering"),
@@ -68,7 +69,7 @@ async def list_tapir_sessions(
     query = TapirSessionModel.base_query(session)
 
     if id is not None:
-        query = query.filter(TapirSession.user_id.in_(id))
+        query = query.filter(TapirSession.session_id.in_(id))
     else:
         if _start < 0 or _end < _start:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
@@ -92,6 +93,12 @@ async def list_tapir_sessions(
 
         if user_id:
             query = query.filter(TapirSession.user_id == user_id)
+
+        if is_open is not None:
+            if is_open:
+                query = query.filter(TapirSession.end_time == 0)
+            else:
+                query = query.filter(TapirSession.end_time != 0)
 
     order_columns = []
     if _sort:
@@ -140,13 +147,15 @@ async def get_tapir_session_for_user(
     return list_tapir_sessions(response, _sort, _order, _start, _end, user_id=user_id, session=session)
 
 
+class TapirSessionUpdateModel(BaseModel):
+    close_session: bool
 
-@router.put("/{id:int}/close")
+@router.put("/{id:int}")
 async def close_tapir_session(
-        request: Request,
         id:int,
+        body: TapirSessionUpdateModel,
         session: Session = Depends(get_db)) -> TapirSessionModel:
-    # This part of logic, I have no clue.
+    # This is part of logic, I have no clue.
     # I think last_reissue can be null (or 0) so not sure how correct this is.
     #
     # 	function _expire_session() {
@@ -161,24 +170,13 @@ async def close_tapir_session(
 	# 	$this->_clear_session_cookie();
 	# }
 
-    tapir_session: TapirSession = session.query(TapirSession).filter(TapirSession.session_id == id).one_or_none()
+    tapir_session: TapirSession | None = session.query(TapirSession).filter(TapirSession.session_id == id).one_or_none()
     if not tapir_session:
         raise HTTPException(status_code=404, detail=f"TapirSession not found for {id}")
-    body = await request.json()
-    if body.get("close_session") and not tapir_session.end_time:
-        cookie_reissue = 30
-        try:
-            cookie_reissue = int(request.app.extra.get("TAPIR_SESSION_COOKIE_REISSUE", "30"))
-        except ValueError:
-            pass
-        if tapir_session.last_reissue:
-            tapir_session.end_time = tapir_session.last_reissue + cookie_reissue
-        elif tapir_session.start_time:
-            tapir_session.end_time = tapir_session.start_time + cookie_reissue
-        else:
-            tapir_session.end_time = datetime.datetime.now().timestamp() + cookie_reissue
+    if body.close_session:
+        if tapir_session.end_time != 0:
+            raise HTTPException(status_code=400, detail=f"TapirSession already closed for {id}")
+        tapir_session.end_time = datetime_to_epoch(None, datetime.datetime.now(datetime.UTC))
         session.commit()
         session.refresh(tapir_session)
-
     return TapirSessionModel.model_validate(TapirSessionModel.base_query(session).filter(TapirSession.session_id == id).one_or_none())
-
