9bf6da5cab53d747040560c46fbe22bc3c61a0c2
Naoyuki Tai <ntai@arxiv.org>
2025-10-01 11:55:50 -0400

    Font change

--- ui-arxiv-admin/src/navTheme.tsx
+++ ui-arxiv-admin/src/navTheme.tsx
@@ -18,45 +18,37 @@ export const BLACK = '#000000';
 // Common base theme (sizes, spacing, behavior)
 const commonBaseTheme = {
     typography: {
-        fontFamily: '"IBM Plex Mono"', // Primary font
+        fontFamily: '"IBM Plex Sans Condensed", sans-serif', // Primary font
         fontSize: 16,
         body1: {
             fontSize: '12px',
-            fontFamily: '"IBM Plex Mono"', // Primary font
         },
         body2: {
             fontSize: '11px',
-            fontFamily: '"IBM Plex Mono"', // Primary font
         },
         h1: {
             fontSize: '40px',
             fontWeight: 700,
-            fontFamily: '"IBM Plex Sans Condensed"', // Secondary font for headers
         },
         h2: {
             fontSize: '24px',
             fontWeight: 700,
-            fontFamily: '"IBM Plex Sans Condensed"', // Secondary font for headers
         },
         h3: {
             fontSize: '22px',
             fontWeight: 700,
-            fontFamily: '"IBM Plex Sans Condensed"', // Secondary font for headers
         },
         h4: {
             fontSize: '20px',
             fontWeight: 700,
-            fontFamily: '"IBM Plex Sans Condensed"', // Secondary font for headers
         },
         h5: {
             fontSize: '18px',
             fontWeight: 700,
-            fontFamily: '"IBM Plex Sans Condensed"', // Secondary font for headers
         },
         h6: {
             fontSize: '16px',
             fontWeight: 700,
-            fontFamily: '"IBM Plex Sans Condensed"', // Secondary font for headers
         },
         // Custom variant for monospace content (System Data, code, etc.)
         monospace: {
