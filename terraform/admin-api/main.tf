b6c9f57c7b0659868bf287f8ae5051cd2dcf7e98
Mike Greco <mike@arxiv.org>
2026-02-19 15:51:02 -0500

    added more variables

--- terraform/admin-api/main.tf
+++ terraform/admin-api/main.tf
@@ -35,6 +35,42 @@ resource "google_secret_manager_secret_iam_member" "jwt_secret_accessor" {
   member    = "serviceAccount:${google_service_account.account.email}"
 }
 
+resource "google_secret_manager_secret_iam_member" "admin_api_shared_secret_accessor" {
+  secret_id = var.admin_api_shared_secret_name
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "pwc_secret_accessor" {
+  secret_id = var.pwc_secret_name
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "pwc_arxiv_user_secret_accessor" {
+  secret_id = var.pwc_arxiv_user_secret_name
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "arxiv_document_bucket_name_accessor" {
+  secret_id = var.arxiv_document_bucket_name
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "admin_api_smtp_url_accessor" {
+  secret_id = var.admin_api_smtp_url_secret_name
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "modapi_modkey_accessor" {
+  secret_id = var.modapi_modkey_secret_name
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.account.email}"
+}
+
 resource "google_project_iam_member" "ar_writer" {
   project = var.gcp_project_id
   role    = "roles/artifactregistry.createOnPushWriter"
@@ -81,6 +117,44 @@ resource "google_cloud_run_v2_service" "admin_api" {
         value = var.admin_api_root_path
       }
       env {
+        name = "ADMIN_API_SHARED_SECRET"
+        value_source {
+          secret_key_ref {
+            secret  = var.admin_api_shared_secret_name
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "ADMIN_API_SMTP_URL"
+        value_source {
+          secret_key_ref {
+            secret  = var.admin_api_smtp_url_secret_name
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name  = "ADMIN_APP_URL"
+        value = var.admin_app_url
+      }
+      env {
+        name  = "AAA_LOGIN_REDIRECT_URL"
+        value = var.aaa_login_redirect_url
+      }
+      env {
+        name  = "AAA_TOKEN_REFRESH_URL"
+        value = var.aaa_token_refresh_url
+      }
+      env {
+        name  = "ARXIV_CHECK_URL"
+        value = var.arxiv_check_url
+      }
+      env {
+        name  = "ARXIV_DOCUMENT_BUCKET_NAME"
+        value = var.arxiv_document_bucket_name
+      }
+      env {
         name  = "CLASSIC_COOKIE_NAME"
         value = var.classic_cookie_name
       }
@@ -98,9 +172,18 @@ resource "google_cloud_run_v2_service" "admin_api" {
         value = var.classic_session_hash
       }
       env {
-        name  = "CLASSIC_SESSION_DURATION"
+        name  = "SESSION_DURATION"
         value = var.session_duration
       }
+      env {
+        name  = "GCP_PROJECT"
+        value = var.gcp_project_id
+      }
+      env {
+        name  = "GCP_PROJECT_ID"
+        value = var.gcp_project_id
+      }
+
       env {
         name = "JWT_SECRET"
         value_source {
@@ -109,6 +192,41 @@ resource "google_cloud_run_v2_service" "admin_api" {
             version = "latest"
           }
         }
+      }
+      env {
+        name  = "MODAPI_URL"
+        value = var.modapi_url
+      }
+      env {
+        name = "MODAPI_MODKEY"
+        value_source {
+          secret_key_ref {
+            secret  = var.modapi_modkey_secret_name
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "PWC_SECRET"
+        value_source {
+          secret_key_ref {
+            secret  = var.pwc_secret_name
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "PWC_ARXIV_USER_SECRET"
+        value_source {
+          secret_key_ref {
+            secret  = var.pwc_arxiv_user_secret_name
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name  = "USER_ACTION_SITE"
+        value = var.user_action_site
       }
       volume_mounts {
         name       = "cloudsql"
--- terraform/admin-api/variables.tf
+++ terraform/admin-api/variables.tf
@@ -58,3 +58,63 @@ variable "image_path" {
   description = "Path to the container image in Artifact Registry"
   type        = string
 }
+
+variable "pwc_secret_name" {
+  description = "Name of the PWC secret"
+  type        = string
+}
+
+variable "pwc_arxiv_user_secret_name" {
+  description = "Name of the PWC arXiv user secret"
+  type        = string
+}
+
+variable "arxiv_check_url" {
+  description = "URL of the arXiv check service"
+  type        = string
+}
+
+variable "arxiv_document_bucket_name" {
+  description = "Name of the arXiv document bucket"
+  type        = string
+}
+
+variable "admin_api_shared_secret_name" {
+  description = "Name of the admin API shared secret"
+  type        = string
+}
+
+variable "admin_api_smtp_url_secret_name" {
+  description = "Name of the admin API SMTP URL secret"
+  type        = string
+}
+
+variable "admin_app_url" {
+  description = "URL of the admin app"
+  type        = string
+}
+
+variable "aaa_login_redirect_url" {
+  description = "URL of the AAA login redirect"
+  type        = string
+}
+
+variable "aaa_token_refresh_url" {
+  description = "URL of the AAA token refresh"
+  type        = string
+}
+
+variable "modapi_url" {
+  description = "URL of the moderation API"
+  type        = string
+}
+
+variable "modapi_modkey_secret_name" {
+  description = "Name of the moderation API modkey secret"
+  type        = string
+}
+
+variable "user_action_site" {
+  description = "Site of the user action"
+  type        = string
+}
